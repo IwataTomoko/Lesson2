b7adbf1d1c19e1ca8681b09afa265bc2b5fc9ef9
IwataTomoko <iwatomo02468@gmail.com>
2022-05-21 16:01:19 +0900

    フォーマットを修正

--- Employee.java
+++ Employee.java
@@ -1,16 +1,16 @@
 public class Employee {
 
-private int id;
-private String name;
-private String gender;
-private int salary;
+    private int id;
+    private String name;
+    private String gender;
+    private int salary;
 
-public Employee(int id,String name,String gender,int salary){
-    this.id = id;
-    this.name = name;
-    this.gender = gender;
-    this.salary = salary;
-}
+    public Employee(int id, String name, String gender, int salary) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.salary = salary;
+    }
 
     public int getId() {
         return id;
